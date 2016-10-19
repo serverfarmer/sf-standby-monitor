0f0e0348c059283787574b8f465a8634cc9b8cca
Tomasz Klim <github@tomaszklim.pl>
2016-10-19 21:44:57 +0000

    disable extension on netbsd

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$OSTYPE" = "netbsd" ]; then
+	echo "skipping standby monitor setup, unsupported system"
+	exit 0
+fi
+
 if ! grep -q /opt/farm/ext/standby-monitor/cron/check.sh /etc/crontab; then
 	sed -i -e "/scripts\/check\/standby.sh/d" /etc/crontab
 	echo "*/30 * * * * root /opt/farm/ext/standby-monitor/cron/check.sh" >>/etc/crontab
