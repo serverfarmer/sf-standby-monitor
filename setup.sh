ddf70eda8a93b5c87a95cb7d67381e63ab182a1a
Tomasz Klim <github@tomaszklim.pl>
2016-10-19 23:51:11 +0200

    removed old transitional code

--- setup.sh
+++ setup.sh
@@ -6,6 +6,5 @@ if [ "$OSTYPE" = "netbsd" ]; then
 fi
 
 if ! grep -q /opt/farm/ext/standby-monitor/cron/check.sh /etc/crontab; then
-	sed -i -e "/scripts\/check\/standby.sh/d" /etc/crontab
 	echo "*/30 * * * * root /opt/farm/ext/standby-monitor/cron/check.sh" >>/etc/crontab
 fi
